97ad89f331f14bef1af6c7b6663defa876d464a6
hamishwaddy <hamishwaddy@gmail.com>
2019-05-07 06:08:20 +1200

    finished PhoneBook console app

---
+++ PhoneBook2/PhoneBook2/Phonebook.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace PhoneBook2
+{
+    public class Phonebook
+    {
+       
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public Address address { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public Company company { get; set; }
+
+
+
+
+
+
+        public List<Phonebook> GetPhoneBook() 
+        {
+            string json = new WebClient().DownloadString("https://jsonplaceholder.typicode.com/users?_limit=5");
+
+            List<Phonebook> users = new JavaScriptSerializer().Deserialize<List<Phonebook>>(json);
+
+            return users;
+
+        }
+
+        public void DisplayPhonebook(Login user)
+        {
+            bool isSupervisor = user.checkSupervisor(user.userName);
+
+            List<Phonebook> users = GetPhoneBook();
+
+            foreach (Phonebook x in users)
+            {
+                Console.Write("Name: {0}\nPhone Number: {1}\nADDRESS:\nStreet: {2}\nCity: {3}\n", x.name, x.phone, x.address.street, x.address.city);
+                if (isSupervisor)
+                {
+                    Console.WriteLine("Email: {0}", x.email);
+                }else
+                    Console.WriteLine(" ");
+            }
+        }
+
+
+    }
+
+
+    public class Geo
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+
+    public class Address
+    {
+        public string street { get; set; }
+        public string suite { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+        public Geo geo { get; set; }
+    }
+
+    public class Company
+    {
+        public string name { get; set; }
+        public string catchPhrase { get; set; }
+        public string bs { get; set; }
+    }
+}
---
+++ PhoneBook2/PhoneBook2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Login currentUser = new Login();
+
+            Console.Write("Please enter your username: ");
+            currentUser.userName= Console.ReadLine();
+            Console.Write("Please enter your password: ");
+            currentUser.password = Console.ReadLine();
+
+            
+            while(!currentUser.UserAuthentication(currentUser.userName, currentUser.password))
+            {
+                Console.WriteLine("Login details incorrect. Please try again");
+                Console.Write("Please enter your password: ");
+                currentUser.password = Console.ReadLine();
+
+            }
+
+            Phonebook phonebookList = new Phonebook();
+            phonebookList.DisplayPhonebook(currentUser);
+
+            Console.ReadLine();
+
+        }
+    }
+}
